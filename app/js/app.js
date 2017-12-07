e4bbd4755becd21a7ec0759b640b156b1f287238
jump85 <giovanni.massafra@ablio.it>
2017-12-07 15:30:14 +0100

    mostra risultati al click del bottone ricerca

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,11 @@
 angular.module('ReSearchApp', []).controller('contentCtrl', function($scope) {
     $scope.titolo = 'Re-Search'
+
+    $scope.mostraRisultati = false;
+
+    $scope.ricerca = function(){
+      $scope.mostraRisultati = true;
+    }
+
+
 });
