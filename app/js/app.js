380a02e53519f0ec3c8e8e716276c96cb57165af
jump85 <giovanni.massafra@ablio.it>
2017-12-12 20:27:18 +0100

    ricerca google custom search implementata

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,28 @@
-angular.module('ReSearchApp', []).controller('contentCtrl', function($scope) {
+angular.module('ReSearchApp', []).controller('contentCtrl', function($scope, $http) {
     $scope.titolo = 'Re-Search'
 
+    $scope.ricerca = {};
     $scope.mostraRisultati = false;
+    $scope.ricerca.testo = '';
 
     $scope.ricerca = function(){
       $scope.mostraRisultati = true;
+
+
+      $http({
+        method: 'GET',
+        url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCeNyIX6Wczm5X2-hIACEOmohWltUvyd-8&cx=017576662512468239146:omuauf_lfve&q='+$scope.ricerca.testo
+      }).then(function successCallback(response) {
+        console.log($scope.ricerca.testo);
+        console.log(response.data.items);
+        $scope.ricerca.risultati = response.data.items;
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
     }
+
+
 
 
 });
